14a2b45c50879f0e286bd394d667cac4e57f10e2
shrikantmergu143 <shriakntmergu143@gmail.com>
2022-10-09 19:05:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const ws = require('ws');
+
+const app = express();
+
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  socket.on('message', function(message, flags){
+    console.log("message", message, flags);
+    socket.send(message);
+  });
+  
+});
+
+const server = app.listen(4000);
+
+server.on('upgrade', (request, socket, head) => {
+    console.log("request, socket, head, socket", request.url)
+    if (request.url === '/foo') {
+      wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+      });
+    }
+});
